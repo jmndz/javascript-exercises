dfeafd88c7ce2ffdd0d755abd5335f09e087f004
jmndz <janaemilyn@gmail.com>
2022-07-27 02:15:24 +0800

    completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    let oldestPerson = 0;
+    let nextOldest = 0;
+    const d = new Date();
+    const oldest = obj.sort(function(a,b){
+        if(!a.yearOfDeath){ 
+            oldestPerson = d.getFullYear() - a.yearOfBirth;
+        }else{
+            oldestPerson = a.yearOfDeath - a.yearOfBirth;
+        }
+        if(!b.yearOfDeath){ 
+            nextOldest = d.getFullYear() - b.yearOfBirth;
+        }else{
+            nextOldest = b.yearOfDeath - b.yearOfBirth;
+        }
+        return oldestPerson > nextOldest ? -1:1;
+    });
+    return oldest[0];
 };
 
+ 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,46 @@
-const caesar = function() {
+const alphabetCapital = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
+const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+const caesar = function(string,num) {
+    let newString = '';
+    for(let i=0;i<string.length;i++){
+        newString += caesarCode(string[i],num);
+    }
+    return newString;
+};
 
+const caesarCode = function(char,num) {
+    let bool = true;
+    let index = 0;
+    let code = char;
+    for(alph of alphabetCapital){
+        if(char === alph){
+            return code = unicode(bool,index+num);
+        }
+        index++;
+    }
+    index = 0;
+    for(alph of alphabet){
+        if(char === alph){
+            bool = false;
+            return code = unicode(bool,index+num);
+        }
+        index++;
+    }
+
+    return char;
+};
+
+const unicode = function(bool,index){
+    for(;;){
+        if(index<0) index += 26;
+        else if(index>25) index -= 26 ;
+        else break;
+    }
+    if(bool === true){
+        return alphabetCapital[index];
+    }else{
+        return alphabet[index];
+    }
 };
 
 // Do not edit below this line
